7897308e7b455604aa5245875feab7e5098bf49e
Jalisa Hewitt <Jalisa@Jalisas-MacBook-Pro.local>
2021-10-26 13:49:49 -0500

    refactored processor, added launch file, and removed prints

--- src/CodeChallenege/api.py
+++ src/CodeChallenege/api.py
@@ -12,5 +12,4 @@ def postData(unique_emails, user_domain_counts, april_emails):
     headers =  {"Content-Type":"application/json"}
     json_data = {"your_email_address": your_email_address, "unique_emails": unique_emails, "user_domain_counts":user_domain_counts, "april_emails": april_emails  }
     response = requests.post(api_url,json.dumps(json_data), headers=headers)
-    print(response)
     return response.status_code
--- src/CodeChallenege/processor.py
+++ src/CodeChallenege/processor.py
@@ -5,11 +5,13 @@ def distinct_emails(user_logs):
     emails = set()
     for user_log in user_logs:
         if user_log['email'] is not None:
-            emails.add(user_log['email'])
+            emails.add(user_log['email'].strip())
     return list(emails)
 
 def domain_counts(emails):
     domains = dict()
+    domains_with_multiple_users = dict()
+
     for email in emails:
         email_split = email.split("@")
         domain = email_split[1]
@@ -17,16 +19,17 @@ def domain_counts(emails):
             domains[domain] = 1
         else:
             domains[domain] += 1
+            domains_with_multiple_users[domain] = domains[domain]
 
-    return domains
+    return domains_with_multiple_users
 
 def april_logins(user_logs):
     emails = set()
     for user_log in user_logs:
-        datetime = user_log['login_date']
-        time = pytz.UTC.normalize(dateutil.parser.parse(datetime))
-        if(4 == time.month):
-            print(datetime)
-            emails.add(user_log['email'])
+        if user_log is not None and user_log['login_date'] is not None and user_log['login_date'] :
+            datetime = user_log['login_date']
+            time = pytz.UTC.normalize(dateutil.parser.parse(datetime))
+            if(4 == time.month):
+                emails.add(user_log['email'].strip())
     return list(emails)
     
